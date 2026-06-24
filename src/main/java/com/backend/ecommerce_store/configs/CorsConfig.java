0802e3d8c8954b5c5f891b70579b8c9b3f673c9d
Piyush Sharma <ps1375982@gmail.com>
2026-06-24 16:55:36 +0530

    Refactor CorsConfig package structure

--- src/main/java/com/backend/ecommerce_store/configs/CorsConfig.java
+++ src/main/java/com/backend/ecommerce_store/configs/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.backend.ecommercebackend.configs;
+package com.backend.ecommerce_store.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
