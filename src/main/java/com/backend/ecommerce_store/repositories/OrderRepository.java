20851247f45a5e374184ea7facaa5b83e16270fc
Piyush Sharma <ps1375982@gmail.com>
2026-06-26 01:15:05 +0530

    Add method to find orders by user ID

--- src/main/java/com/backend/ecommerce_store/repositories/OrderRepository.java
+++ src/main/java/com/backend/ecommerce_store/repositories/OrderRepository.java
@@ -2,7 +2,8 @@ package com.backend.ecommerce_store.repositories;
 
 import com.backend.ecommerce_store.models.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-//    public Order placeOrder(Integer userId);
+  List<Order> findByUserId(Integer userId);
 }
