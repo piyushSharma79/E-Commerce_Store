cbc181a21c6d2b2fe6506f48e2e055e2e8f2fe7b
Piyush Sharma <ps1375982@gmail.com>
2026-06-26 01:16:18 +0530

    Update OrderService.java
    Added import statement for List to OrderService.

--- src/main/java/com/backend/ecommerce_store/services/OrderService.java
+++ src/main/java/com/backend/ecommerce_store/services/OrderService.java
@@ -6,6 +6,7 @@ import com.backend.ecommerce_store.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.time.LocalDateTime;
 
 @Service
